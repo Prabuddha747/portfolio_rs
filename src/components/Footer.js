75e3215deee6ff756e2963475ce879276d911313
Prabuddha747 <111564174+Prabuddha747@users.noreply.github.com>
2024-12-31 12:23:44 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="www.linkedin.com/in/prabuddha-verma-8647a6265"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/prabuddha-verma-8647a6265"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://github.com/Prabuddha747?tab=repositories"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://www.instagram.com/prabuddha_verma/"><img src={navIcon3} alt="Icon" /></a>
             </div>
