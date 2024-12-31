4e8b395974b72ebbf6e29bdfca162dc4a41498d4
Prabuddha747 <111564174+Prabuddha747@users.noreply.github.com>
2024-12-31 12:25:51 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,14 +12,31 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/prabuddha-verma-8647a6265"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://github.com/Prabuddha747?tab=repositories"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/prabuddha_verma/"><img src={navIcon3} alt="Icon" /></a>
+              <a 
+                href="https://www.linkedin.com/in/prabuddha-verma-8647a6265" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn Icon" />
+              </a>
+              <a 
+                href="https://github.com/Prabuddha747?tab=repositories" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon2} alt="GitHub Icon" />
+              </a>
+              <a 
+                href="https://www.instagram.com/prabuddha_verma/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon3} alt="Instagram Icon" />
+              </a>
             </div>
-            
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
